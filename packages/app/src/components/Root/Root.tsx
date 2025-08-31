54a626e8e8c283b560d5b81f70d5754502c4790c
Kriss683 <shumway_kristin14@yahoo.com>
2025-08-31 09:19:15 -0400

    Customizing UI

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -1,99 +1,95 @@
 import { PropsWithChildren } from 'react';
 import { makeStyles } from '@material-ui/core';
-import HomeIcon from '@material-ui/icons/Home';
-import ExtensionIcon from '@material-ui/icons/Extension';
-import LibraryBooks from '@material-ui/icons/LibraryBooks';
-import CreateComponentIcon from '@material-ui/icons/AddCircleOutline';
 import LogoFull from './LogoFull';
-import LogoIcon from './LogoIcon';
 import {
   Settings as SidebarSettings,
   UserSettingsSignInAvatar,
 } from '@backstage/plugin-user-settings';
-import { SidebarSearchModal } from '@backstage/plugin-search';
 import {
-  Sidebar,
-  sidebarConfig,
-  SidebarDivider,
-  SidebarGroup,
-  SidebarItem,
-  SidebarPage,
-  SidebarScrollWrapper,
-  SidebarSpace,
-  useSidebarOpenState,
   Link,
 } from '@backstage/core-components';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import { MyGroupsSidebarItem } from '@backstage/plugin-org';
-import GroupIcon from '@material-ui/icons/People';
 import { NotificationsSidebarItem } from '@backstage/plugin-notifications';
 
-const useSidebarLogoStyles = makeStyles({
+const useHeaderStyles = makeStyles({
   root: {
-    width: sidebarConfig.drawerWidthClosed,
-    height: 3 * sidebarConfig.logoHeight,
     display: 'flex',
-    flexFlow: 'row nowrap',
-    alignItems: 'center',
-    marginBottom: -14,
+    flexDirection: 'column',
+    minHeight: '100vh',
+    width: '100%',
   },
-  link: {
-    width: sidebarConfig.drawerWidthClosed,
-    marginLeft: 24,
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '0 24px',
+    height: 72,
+    borderBottom: '3px solid #e61616',
+    width: '100%',
+    backgroundColor: '#ffffff',
+    boxSizing: 'border-box',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+  },
+  leftSection: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: 48,
+  },
+  navigation: {
+    display: 'flex',
+    gap: 32,
+    alignItems: 'center',
+  },
+  navItem: {
+    textDecoration: 'none',
+    color: '#000000',
+    padding: '12px 20px',
+    borderRadius: 4,
+    fontWeight: 500,
+    fontSize: '16px',
+    transition: 'all 0.2s ease',
+    '&:hover': {
+      backgroundColor: '#e61616',
+      color: '#ffffff',
+    },
+  },
+  rightSection: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: 16,
+  },
+  content: {
+    flex: 1,
+    width: '100%',
   },
 });
 
-const SidebarLogo = () => {
-  const classes = useSidebarLogoStyles();
-  const { isOpen } = useSidebarOpenState();
+export const Root = ({ children }: PropsWithChildren<{}>) => {
+  const classes = useHeaderStyles();
 
   return (
     <div className={classes.root}>
-      <Link to="/" underline="none" className={classes.link} aria-label="Home">
-        {isOpen ? <LogoFull /> : <LogoIcon />}
-      </Link>
+      <div className={classes.header}>
+        <div className={classes.leftSection}>
+          <Link to="/" underline="none" aria-label="Home">
+            <LogoFull />
+          </Link>
+          <nav className={classes.navigation}>
+            <Link to="/catalog" className={classes.navItem}>Home</Link>
+            <Link to="/api-docs" className={classes.navItem}>APIs</Link>
+            <Link to="/docs" className={classes.navItem}>Docs</Link>
+            <Link to="/create" className={classes.navItem}>Create</Link>
+            <Link to="/search" className={classes.navItem}>Search</Link>
+          </nav>
+        </div>
+        <div className={classes.rightSection}>
+          <NotificationsSidebarItem />
+          <UserSettingsSignInAvatar />
+          <SidebarSettings />
+        </div>
+      </div>
+      <div className={classes.content}>
+        {children}
+      </div>
     </div>
   );
 };
-
-export const Root = ({ children }: PropsWithChildren<{}>) => (
-  <SidebarPage>
-    <Sidebar>
-      <SidebarLogo />
-      <SidebarGroup label="Search" icon={<SearchIcon />} to="/search">
-        <SidebarSearchModal />
-      </SidebarGroup>
-      <SidebarDivider />
-      <SidebarGroup label="Menu" icon={<MenuIcon />}>
-        {/* Global nav, not org-specific */}
-        <SidebarItem icon={HomeIcon} to="catalog" text="Home" />
-        <MyGroupsSidebarItem
-          singularTitle="My Group"
-          pluralTitle="My Groups"
-          icon={GroupIcon}
-        />
-        <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
-        <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
-        <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
-        {/* End global nav */}
-        <SidebarDivider />
-        <SidebarScrollWrapper>
-          {/* Items in this group will be scrollable if they run out of space */}
-        </SidebarScrollWrapper>
-      </SidebarGroup>
-      <SidebarSpace />
-      <SidebarDivider />
-      <NotificationsSidebarItem />
-      <SidebarDivider />
-      <SidebarGroup
-        label="Settings"
-        icon={<UserSettingsSignInAvatar />}
-        to="/settings"
-      >
-        <SidebarSettings />
-      </SidebarGroup>
-    </Sidebar>
-    {children}
-  </SidebarPage>
-);
